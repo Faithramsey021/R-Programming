79eceb817f0cb4e2a1985504d2844fe76e3890f8
Faith Ramsey <121475898+Faithramsey021@users.noreply.github.com>
2023-02-08 16:34:53 -0800

    Add files via upload

--- Peer-graded Assignment_Programming Assignment 2_Lexical Scoping.R
+++ Peer-graded Assignment_Programming Assignment 2_Lexical Scoping.R
@@ -1,71 +1,46 @@
- makeVector <- function(x = numeric()) {
+> makeVector <-function(x = numeric()) {
 + m <- NULL
 + set <- function(y) {
 +     x <<- y
 +     m <<- NULL
 + }
-+ 
 + get <- function() x
-+ setmean <- function(mean) m <<- mean
-+ getmean <- function() m
-+ list(set = set, get = get,
-+     setmean = setmean,
-+     getmean = getmean)
++ setmean <-function(mean) m <<- mean
++ list (set = set, get = get,
++       setmean = setmean,
++       getmean = getmean)
 + }
 > cachemean <- function(x, ...) {
 + m <- x$getmean()
 + if(!is.null(m)) {
-+     message("getting cached data")
-+     return(m)
++ message ("getting cached data")
++ return(m)
 + }
 + data <- x$get()
 + m <- mean(data, ...)
 + x$setmean(m)
 + m
 + }
-
-> makeCacheMatrix <- function(x=matrix) {
+> makeCacheMatrix <- function(x = matrix()) {
++ m <- NULL
++ set <- function(y) {
++     x <<- y
++ }
++ get <- function() x
++ setinverse <- function(inverse) m <<- invr
++ getinverse <- function() m
++ list (set = set, get = get,
++       setinverse = setinverse,
++       getinverse = getinverse)
 + }
 > cacheSolve <- function(x, ...) {
-+ }
-> makeCacheMatrix <- function(x=matrix()){
-+ invr<-NULL
-+ set<-function(y){
-+ x<<-y
-+ invr<<-NULL
-+ }
-+ }
-> get <- function() x
-> setinverse <- function(inverse) m <<- inverse
-> getinverse <- function() m
-> list(set = set, get = get,
-+      setinverse = setinverse,
-+      getinverse = getinverse)
-$set
-function(inverse) invr<<-inverse
-
-$get
-function() x
-
-$setinverse
-function(inverse) m <<- inverse
-
-$getinverse
-function() m
-
-> cacheSolve <- function(x, ...) {
-+ m<= x$getinverse()
++ m <- x$getinverse()
 + if(!is.null(m)) {
-+ message("Getting Cached Data:-")
++ message("getting cached data")
 + return(m)
 + }
-+ matrx<-x$get()
-+ m<-solve(matrx, ...)
++ mat <- x$get()
++ m <- solve(mat, ...)
 + x$setinverse(m)
 + m
 + }
-> solve
-function (a, b, ...) 
-UseMethod("solve")
-<bytecode: 0x0000026651b753b8>
-<environment: namespace:base>
